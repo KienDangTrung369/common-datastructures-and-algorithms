56a71dc0c337436104a38359f983d54e8846f97b
KienDangTrung369 <dtkien369@gmail.com>
2022-02-18 11:45:28 +0700

    update zfunction

--- String/zfunction.cpp
+++ String/zfunction.cpp
@@ -5,7 +5,8 @@ typedef long long ll;
 typedef long double lld;
 typedef unsigned long long ull;
 const ll inf = 1e18;
-
+const int N = 1e6 + 10;
+int failure[N];
 using namespace std;
 vector<int> zfunction(const string& s) {
   int n = s.size();
@@ -21,6 +22,30 @@ vector<int> zfunction(const string& s) {
     }
   }
   return z;
+}
+void build_failure_function(string pattern, int m) {
+  failure[0] = 0;
+  failure[1] = 0; //base case
+
+  for(int i = 2; i <= m; i++) {  //i is length of the prefix we are dealing with
+    // j is the index of the largest next partial match
+    // (the largest suffix/prefix) of the string under index i - 1
+    int j = failure[i - 1];
+    while(true) {
+      // check if the last character of prefix of length i "expands" the current "candidate"
+      if(pattern[j] == pattern[i - 1]) {
+        failure[i] = j + 1;
+        break;
+      }
+      // if we cannot "expand" even the empty string
+      if(j == 0) {
+          failure[i] = 0;
+          break;
+      }
+      // else go to the next best "candidate" partial match
+      j = failure[j];
+    }
+  }
 }
 int main() {
  // freopen("file.in", "r", stdin);
